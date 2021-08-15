fa38189e1c353a4f58162213bd0f5ca4c3e82ddc
OlViMshv <88428097+OlViMshv@users.noreply.github.com>
2021-08-15 23:43:25 +0500

    Add files via upload

---
+++ lesson_2_homework.sql
@@ -0,0 +1,81 @@
+--схема БД: https://docs.google.com/document/d/1NVORWgdwlKepKq_b8SPRaSpraltxoMg2SIusTEN6mEQ/edit?usp=sharing
+--colab/jupyter: https://colab.research.google.com/drive/1j4XdGIU__NYPVpv74vQa9HUOAkxsgUez?usp=sharing
+
+Задание 1: Вывести name, class по кораблям, выпущенным после 1920
+select name, class 
+from ships
+where launched > 1920
+
+Задание 2: Вывести name, class по кораблям, выпущенным после 1920, но не позднее 1942
+select name, class 
+from ships
+where launched > 1920 and launched < 1942
+
+Задание 3: Какое количество кораблей в каждом классе. Вывести количество и class
+select count (name), class 
+from ships s 
+group by class
+
+Задание 4: Для классов кораблей, калибр орудий которых не менее 16, укажите класс и страну. (таблица classes)
+select class, country 
+from classes
+where bore >=16
+
+Задание 5: Укажите корабли, потопленные в сражениях в Северной Атлантике (таблица Outcomes, North Atlantic). Вывод: ship.
+select ship 
+from Outcomes
+where result ='sunk' and battle = 'North Atlantic'
+Задание 6: Вывести название (ship) последнего потопленного корабля
+
+select ship 
+from 
+(
+select *
+from outcomes o
+join battles b 
+on o.battle = b.name
+where o.result = 'sunk' and date in (
+	select max(date)
+	from outcomes o1
+	join battles b 
+	on o1.battle = b.name)
+)t1
+
+Задание 7: Вывести название корабля (ship) и класс (class) последнего потопленного корабля
+select ship, coalesce (class, '-')
+from 
+(
+select *
+from outcomes o
+left join battles b 
+on o.battle = b.name
+left join ships s 
+on o.ship = s.name
+where o.result = 'sunk' and date in (
+	select max(date)
+	from outcomes o1
+	join battles b 
+	on o1.battle = b.name)
+)t1
+
+
+Задание 8: Вывести все потопленные корабли, у которых калибр орудий не менее 16, и которые потоплены. Вывод: ship, class
+select o.ship, s.class
+from classes c 
+join ships s 
+on c.class = s.class
+join outcomes o 
+on s.name = o.ship
+where o.result = 'sunk' and bore >=16
+
+Задание 9: Вывести все классы кораблей, выпущенные США (таблица classes, country = 'USA'). Вывод: class
+select class
+from classes c 
+where country = 'USA'
+
+Задание 10: Вывести все корабли, выпущенные США (таблица classes & ships, country = 'USA'). Вывод: name, class
+select name, s.class
+from classes c 
+join ships s 
+on c.class = s.class
+where country = 'USA'
