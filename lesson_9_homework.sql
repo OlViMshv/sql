ecef493d41b6f97cf36f3001e576b10a256962c6
OlViMshv <88428097+OlViMshv@users.noreply.github.com>
2021-09-25 22:12:21 +0500

    Add files via upload

---
+++ lesson_9_homework.sql
@@ -0,0 +1,24 @@
+--task1  (lesson9)
+-- oracle: https://www.hackerrank.com/challenges/the-report/problem
+
+--task2  (lesson9)
+-- oracle: https://www.hackerrank.com/challenges/occupations/problem
+
+--task3  (lesson9)
+-- oracle: https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+
+--task4  (lesson9)
+-- oracle: https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+
+--task5  (lesson9)
+-- oracle: https://www.hackerrank.com/challenges/weather-observation-station-11/problem
+
+--task6  (lesson9)
+-- oracle: https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+
+--task7  (lesson9)
+-- oracle: https://www.hackerrank.com/challenges/salary-of-employees/problem
+select name from Employee where salary > 2000 and months < 10 order by employee_id ;
+--task8  (lesson9)
+-- oracle: https://www.hackerrank.com/challenges/the-report/problem
+--повтор первого задания!
